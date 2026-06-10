375ecb8bb69c796b47996f67108f1b04bb946ecc
Amir B. Kahna <amir.kahna2@gmail.com>
2026-06-10 16:51:42 +0200

    Adding ex1-2-3

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import entities.Customer;
+import entities.Order;
+import entities.Product;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+void main(String[] args) {
+
+    Customer Alexander = new Customer(523523L, "Alexander", 1);
+    Customer Martina = new Customer(33423141L, "Martina", 2);
+    Customer Paolone = new Customer(346346L, "Paolo", 3);
+    Product hp1 = new Product(123456L, "Harry Potter and the Philosopher's Stone.", "Books", 59.99);
+    Product hp2 = new Product(233455L, "Harry Potter and the Chamber of Secrets.", "Books", 119.99);
+    Product hp3 = new Product(252414L, "Harry Potter and the Prisoner of Azkaban.", "Books", 99.99);
+    Product bike = new Product(430943L, "Kawasaki Ninja ZX-10R", "Boys", 18.990);
+    Product cream = new Product(252352L, "Sol de Janeiro", "Girls", 41.99);
+    Product pacifier = new Product(424234L, "Philips Soothie", "Baby", 15.99);
+    Product stroller = new Product(665243L, "Stokke YOYO 3", "Baby", 499.99);
+
+
+    List<Product> products = List.of(hp1, hp2, hp3, bike, cream, pacifier, stroller);
+
+
+    List<Product> result = products.stream().filter(p -> p.getCategory().equals("Books")).filter(p -> p.getPrice() > 100).toList();
+    System.out.println("Book price over 100:");
+    System.out.println(result);
+
+    Order order1 = new Order(235235L, "Shipped", LocalDate.now(), LocalDate.now().plusDays(2), List.of(bike, stroller, hp1), Alexander);
+    Order order2 = new Order(325235L, "Pending", LocalDate.now(), LocalDate.now().plusDays(7), List.of(cream, pacifier, hp2), Martina);
+    Order order3 = new Order(5235232L, "Out for delivery", LocalDate.now(), LocalDate.now(), List.of(hp3, bike), Paolone);
+
+    List<Order> orders = List.of(order1, order2, order3);
+
+    List<Order> babyCategory = orders.stream().filter(order -> order.getProducts().stream().anyMatch(product -> product.getCategory().equals("Baby"))).toList();
+    System.out.println("\nOrders that include 'Baby' category items:");
+    System.out.println(babyCategory);
+
+    List<Product> boys10 = products.stream().filter(p -> p.getCategory().equals("Boys")).map(p -> new Product(p.getId(), p.getName(), p.getCategory(), p.getPrice() * 0.9)).toList();
+    System.out.println("\n10% off for items in Boys category:");
+    System.out.println(boys10);
 }
+
+
+
